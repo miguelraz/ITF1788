d36b4e1a621b9e37d01e58a13fedb3530e783d66
Oliver Heimlich <oheim@posteo.de>
2015-08-30 20:14:35 +0200

    defaultGenerator.sh: Simplified calling the package without prior build

--- defaultGenerator.sh
+++ defaultGenerator.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-python3 setup.py build
-
-export PYTHONPATH=build/lib
-
 if [ ! "$@" ]; then
-    python3 -m itf1788 -s itl -o output
+    python3 -m src -s itl -o output
 else
-    python3 -m itf1788 "@*"
+    python3 -m src "@*"
 fi
